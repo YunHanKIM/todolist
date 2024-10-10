0316eaa9b6a3c8711e3b0fe42fce6e28eda6f3c8
김윤한 <dbsgks19@naver.com>
2024-10-10 22:30:47 +0900

    filter

--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: "123", text: "장보기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
@@ -10,10 +10,11 @@ export default function TodoList() {
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) => setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
+  const filtered = getFilteredItems(filter, todos);
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -25,4 +26,11 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+function getFilteredItems(filter, todos) {
+  if (filter === "all") {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
